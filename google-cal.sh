00db568dfd881f0078a512897f34b579c4a7cd35
Shane Panter <shane@foundationcode.com>
2024-01-02 14:23:15 -0700

    Update with cal sync scripts

---
+++ google-cal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# customize these
+WGET=<path to wget>
+ICS2ORG=<path to ical2org>
+ICSFILE=<path for icsfile>
+ORGFILE=<path to orgfile>
+URL=<url to your private Google calendar>
+
+# no customization needed below
+
+$WGET -O $ICSFILE $URL
+$ICS2ORG < $ICSFILE > $ORGFILE
--- install.sh
+++ install.sh
@@ -17,6 +17,12 @@ git config --global user.email "shane@foundationcode.com"
 git config --global user.name "Shane Panter"
 
 ln -s `pwd`/mbsyncrc $HOME/.mbsyncrc
+ln -s `pwd`/ical2org.awk $HOME/bin/ical2org
 
+
+echo "Install calendar sync daemon"
+mkdir -p ~/Library/LaunchAgents
+cp launchd.google-calendar-sync.plist ~/Library/LaunchAgents/launchd.google-calendar-sync.plist
+launchctl load -w ~/Library/LaunchAgents/launched.google-calendar-sync.plist
 
 echo "Restart bash or source the bashrc to get all changes"
